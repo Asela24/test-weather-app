915a1ab47a510fe15a0246fbd3106ec01fa6b760
Adelina <adeline.selivanovskaia@toptal.com>
2025-02-09 20:39:19 +0100

    Update

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -4,7 +4,7 @@ export const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 export const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const backgroundStyles: Record<WeatherCondition, string>  = {
-  Clear: "linear-gradient(to top, #00bfff, #1e90ff)",
+  Clear: "linear-gradient(to top,rgb(121, 165, 206), #1e90ff)",
   Clouds: "linear-gradient(to top,rgb(16, 90, 187), #d3d3d3)",
   Rain: "linear-gradient(to top, #708090, #2f4f4f)",
   Snow: "linear-gradient(to top, #00f5ff, #f0f8ff)",
