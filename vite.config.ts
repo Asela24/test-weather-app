a75c602db5124660bfc377122c8276f5bbcb507f
Adelina Selivanovskaia <felmmark@gmail.com>
2025-02-09 19:53:02 +0100

    Create main.yml

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import process from 'process'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/test-weather-app/',
   define: {
     'import.meta.env.ENV_VARIABLE': JSON.stringify(process.env.ENV_VARIABLE)
   }
